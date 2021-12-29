2254c78e257300228338908ee1738995d487793d
Yuvraj <code@evalsocket.dev>
2021-12-29 19:55:29 +0530

    wip: neptune added
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- workflows/imdb.py
+++ workflows/imdb.py
@@ -1,17 +1,39 @@
 import os
 import re
 import string
-import pathlib
 import flytekit
-from flytekit import task, workflow
+import glob
+from flytekit import task, workflow, Secret, Resources
 from flytekit.types.directory import FlyteDirectory
 from flytekit.remote import FlyteRemote
-from flytekit.models.core.execution import WorkflowExecutionPhase
+from flytekitplugins.kftensorflow import TfJob
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow.python.data.ops.dataset_ops import BatchDataset
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import neptune.new as neptune
+from neptune.new.integrations.tensorflow_keras import NeptuneCallback
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+SECRET_NAME = "user_secret"
+SECRET_GROUP = "user-info"
+
+resources = Resources(
+    gpu="2", mem="10Gi", storage="10Gi", ephemeral_storage="500Mi"
+)
+
+
+@dataclass_json
+@dataclass
+class Hyperparameters(object):
+    batch_size_per_replica: int = 64
+    seed: int = 10000
+    epochs: int = 10
+    max_features: int = 1000
+    sequence_length: int = 4
+    embedding_dim: int = 16
 
 
 class Dataset:
@@ -19,42 +41,6 @@ class Dataset:
         self.train_data = train_data
         self.val_data = val_data
         self.test_data = test_data
-
-
-@task
-def download_model(uri: str) -> FlyteDirectory:
-    data = tf.keras.utils.get_file(
-        "aclImdb_v1",
-        uri,
-        untar=True,
-        cache_dir='.',
-        cache_subdir='')
-    working_dir = flytekit.current_context().working_directory
-    return FlyteDirectory(path=os.path.join(working_dir, "aclImdb_v1"))
-
-
-@task
-def get_dataset(data_dir: FlyteDirectory, batch_size: int, seed: int) -> Dataset:
-    raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
-        os.path.join(os.path.dirname(data_dir.path), 'train'),
-        batch_size=batch_size,
-        validation_split=0.2,
-        subset='training',
-        seed=seed
-    )
-    raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
-        os.path.join(os.path.dirname(data_dir.path), 'train'),
-        batch_size=batch_size,
-        validation_split=0.2,
-        subset='validation',
-        seed=seed
-    )
-    raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
-        str(os.path.join(os.path.dirname(data_dir.path), 'test')),
-        batch_size=batch_size
-    )
-
-    return Dataset(raw_train_ds, raw_val_ds, raw_test_ds)
 
 
 def custom_standardization(input_data: str):
@@ -77,10 +63,54 @@ class VectorizedLayer:
         return self.vectorize_layer(text), label
 
 
-@task
-def prepare_dataset(data_set: Dataset, max_features: int, sequence_length: int) -> VectorizedLayer:
+def get_dataset(data_dir: FlyteDirectory, hyperparameters: Hyperparameters) -> Dataset:
+    raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        os.path.join(os.path.dirname(data_dir.path), 'train'),
+        batch_size=hyperparameters.batch_size_per_replica,
+        validation_split=0.2,
+        subset='training',
+        seed=hyperparameters.seed
+    )
+    raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        os.path.join(os.path.dirname(data_dir.path), 'train'),
+        batch_size=hyperparameters.batch_size_per_replica,
+        validation_split=0.2,
+        subset='validation',
+        seed=hyperparameters.seed
+    )
+    raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        str(os.path.join(os.path.dirname(data_dir.path), 'test')),
+        batch_size=hyperparameters.batch_size_per_replica
+    )
+
+    return Dataset(raw_train_ds, raw_val_ds, raw_test_ds)
+
+
+@task(
+    retries=2,
+    cache=True,
+    cache_version="1.0",
+)
+def download_dataset(uri: str) -> Dataset:
+    data = tf.keras.utils.get_file(
+        "aclImdb_v1",
+        uri,
+        untar=True,
+        cache_dir='.',
+        cache_subdir='')
+    return FlyteDirectory(path=os.path.join(os.path.dirname(data), "aclImdb_v1"))
+
+
+@task(
+    retries=2,
+    cache=True,
+    cache_version="1.0",
+)
+def prepare_dataset(data_dir: FlyteDirectory, hyperparameters: Hyperparameters) -> (VectorizedLayer, Dataset):
+    data_set = get_dataset(data_dir=data_dir, hyperparameters=hyperparameters)
+
     train_text = data_set.train_data.map(lambda x, y: x)
-    vectorized_layer = VectorizedLayer(max_features=max_features, sequence_length=sequence_length)
+    vectorized_layer = VectorizedLayer(max_features=hyperparameters.max_features, sequence_length=hyperparameters.sequence_length)
     vectorized_layer.adapt(train_text)
 
     data_set.train_data = data_set.train_data.map(vectorized_layer.vectorized_text)
@@ -92,15 +122,32 @@ def prepare_dataset(data_set: Dataset, max_features: int, sequence_length: int) 
     data_set.val_data = data_set.val_data.cache().prefetch(buffer_size=autotune)
     data_set.test_data = data_set.test_data.cache().prefetch(buffer_size=autotune)
 
-    return vectorized_layer
+    return vectorized_layer,data_set
+
+@task(
+    task_config=TfJob(num_workers=2, num_ps_replicas=1, num_chief_replicas=1),
+    retries=2,
+    secret_requests=[Secret(group=SECRET_GROUP, key=SECRET_NAME)],
+    cache=True,
+    cache_version="1.0",
+    requests=resources,
+    limits=resources,
+)
+def create_model(data_set: Dataset, hyperparameters: Hyperparameters, vectorized_layer: VectorizedLayer) \
+        -> FlyteDirectory:
+
+    run = neptune.init(
+        project="evalsocket/flyte-pipeline",
+        api_token=flytekit.current_context().secrets.get(SECRET_GROUP, SECRET_NAME),
+    )
 
 
-@task
-def create_model(data_set: Dataset, epochs: int, max_features: int, vectorized_layer: VectorizedLayer) \
-        -> (FlyteDirectory, float, float):
-    embedding_dim = 16
+    params = {"max_features": hyperparameters.max_features, "optimizer": "Adam", "epochs": hyperparameters.epochs, "embedding_dim": hyperparameters.embedding_dim}
+    run["parameters"] = params
+    neptune_cbk = NeptuneCallback(run=run, base_namespace="training")
+
     model = tf.keras.Sequential([
-        layers.Embedding(max_features + 1, embedding_dim),
+        layers.Embedding(hyperparameters.max_features + 1, hyperparameters.embedding_dim),
         layers.Dropout(0.2),
         layers.GlobalAveragePooling1D(),
         layers.Dropout(0.2),
@@ -111,11 +158,15 @@ def create_model(data_set: Dataset, epochs: int, max_features: int, vectorized_l
     _ = model.fit(
         data_set.train_data,
         validation_data=data_set.val_data,
-        epochs=epochs)
+        epochs=hyperparameters.epochs,
+        callbacks=[neptune_cbk],
+    )
+    model.save('imdb')
+    run['imdb/saved_model'].upload('imdb/saved_model.pb')
+    for name in glob.glob('imdb/variables/*'):
+        run[name].upload(name)
 
     working_dir = flytekit.current_context().working_directory
-    pp = pathlib.Path(os.path.join(working_dir, "model"))
-    pp.mkdir(exist_ok=True)
 
     export_model = tf.keras.Sequential([
         vectorized_layer,
@@ -126,13 +177,17 @@ def create_model(data_set: Dataset, epochs: int, max_features: int, vectorized_l
     export_model.compile(
         loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy'])
 
-    loss, accuracy = export_model.evaluate(data_set.test_data)
+    eval_metrics = export_model.evaluate(data_set.test_data)
 
-    return FlyteDirectory(path=os.path.join(working_dir, "model")), loss, accuracy
+    for j, metric in enumerate(eval_metrics):
+        run["eval/{}".format(model.metrics_names[j])] = metric
+
+    return FlyteDirectory(path=os.path.join(working_dir, "imdb"))
+
 
 @task
-def server_model_server(domain: str, project: str) \
-        # TODO: Add logic fro getting output of a execution and use it for serving
+def server_model_server(domain: str, project: str):
+        # TODO: Add logic for getting output of a execution and use it for serving
         remote = FlyteRemote.from_config(
             default_project=project,
             default_domain=domain,
@@ -141,21 +196,22 @@ def server_model_server(domain: str, project: str) \
 
 
 @workflow
-def train_and_export(uri: str, batch_size: int, seed: int, max_features: int, sequence_length: int, epochs: int) \
-        -> (FlyteDirectory, float, float):
-    data = download_model(uri=uri)
-    data_set = get_dataset(data_dir=data, batch_size=batch_size, seed=seed)
-    vectorized_layer = prepare_dataset(
-        data_set=data_set, max_features=max_features, sequence_length=sequence_length)
-    model, lost, accuracy = create_model(data_set=data_set, epochs=epochs, max_features=max_features,
+def train_and_export(uri: str, hyperparameters: Hyperparameters = Hyperparameters()) \
+        -> FlyteDirectory:
+    data = download_dataset(uri=uri)
+    vectorized_layer, data_set = prepare_dataset(
+        data_dir=data, hyperparameters=hyperparameters)
+    model = create_model(data_set=data_set, hyperparameters=hyperparameters,
                                          vectorized_layer=vectorized_layer)
-    return model, lost, accuracy
+    return model
+
 
 @workflow
-def serve():
-    server_model_server()
+def serve(domain: str, project: str):
+    server_model_server(domain=domain,project=project)
+
 
 if __name__ == "__main__":
     print(f"Running {__file__} main...")
     print(
-        f"Running chain_tasks_wf()... {train_and_export(uri='https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz', batch_size=5, seed=7)}")
+        f"Running chain_tasks_wf()... {train_and_export(uri='https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz')}")
